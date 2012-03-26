5950ebc22115cee8cd70ff6ede9e151017562de8
Ian <ian@linkportfolio.net>
2012-03-26 12:34:27 -0700

    initial commit, no examples yet

---
+++ hash_retainer.js
@@ -0,0 +1,45 @@
+/*
+This function allows one to update the URL hash without disrupting any other parameters.
+ */
+
+var Hash_retainer = function(options){
+    var build_tab_hash = function(property, value, hash){
+        if(hash.substr(0,1) === '&'){
+            hash = hash.substr(1, hash.length);
+        }
+        if(typeof hash !== 'undefined' && hash !== ''){
+            hash = '#'+property + "=" + value + '&' + hash;
+        }
+        else {
+            hash = '#'+property + "=" + value;
+        }
+        return hash;
+    }
+
+    var prior_tab_hash = function(hash, value){
+        var search = new RegExp("tab="+value, "g");
+        hash = hash.replace(search, '');
+        return hash;
+    }
+
+    this.set_hash = function(map){
+        var url = $.url();
+        var hash = window.location.hash;
+        hash = hash.substr('1', hash.length); // remove the '#'
+
+        $.each(map, function(property, value){
+            var property_hash = url.fparam(property);
+
+            if(hash === ''){
+                hash = build_tab_hash(property, value);
+            } else if(typeof property_hash !== 'undefined'){
+                hash = prior_tab_hash(hash, property_hash);
+                hash = build_tab_hash(property, value, hash);
+            } else {
+                hash = prior_tab_hash(hash, value);
+                hash = build_tab_hash(property, value, hash);
+            }
+        });
+        window.location.hash = hash;
+    }
+}
